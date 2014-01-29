7086febf79f9734e440e3ac82f61b30ea59cf98e
lan.612zhou@gmail.com <lan.612zhou@gmail.com@3b07b1eb-9422-c940-ca5b-aeba0cd4fc7e>
2014-01-29 18:13:03 +0000

    update

--- rppc/src/rppc/main/Main.java
+++ rppc/src/rppc/main/Main.java
@@ -11,19 +11,19 @@ import rppc.vue.Fenetre;
 public class Main {
 
 	public static void main(String[] args) {
-		Probleme p = Probleme.problemeEnonce();
-		Codage c = Codage.permutationEnonce();
-		Decodage d = new Decodage(c, p);
-		d.decoder().affichageSolution();
-		Codage c2 = new Codage(d.decoder());
-		c2.afficher();
-		Solution s = new Solution(p);
-		s.solutionRealisable(false);
-		s.affichageSolution();
-		Codage c3 = new Codage(s);
-		c3.afficher();
-		Decodage d2 = new Decodage(c3, p);
-		d2.decoder().affichageSolution();
+//		Probleme p = Probleme.problemeEnonce();
+//		Codage c = Codage.permutationEnonce();
+//		Decodage d = new Decodage(c, p);
+//		d.decoder().affichageSolution();
+//		Codage c2 = new Codage(d.decoder());
+//		c2.afficher();
+//		Solution s = new Solution(p);
+//		s.solutionRealisable(false);
+//		s.affichageSolution();
+//		Codage c3 = new Codage(s);
+//		c3.afficher();
+//		Decodage d2 = new Decodage(c3, p);
+//		d2.decoder().affichageSolution();
 		
 		IG();
 	}
